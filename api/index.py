44113ea682bce3d0cef2c3f9728b4d858185a16b
Shogo Takata <peshogo+github.com@gmail.com>
2022-12-19 12:01:06 +0900

    print

--- api/index.py
+++ api/index.py
@@ -3,6 +3,7 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        print("request from", self.client_address)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
